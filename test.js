4a1e174128df3632c627e19152001599c57f8814
hsahu1726 <hsahu1726@gmail.com>
2025-07-12 15:14:48 +0530

    datatypes added

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-console.log("Harsh")
-let accEm="hsasdfg"
-const accId="12345"
-var accPwd="qwerty"
-let accState
-console.table([accEm,accId,accPwd,accState]);
-alert(6+8)
+// console.log("Harsh")
+// let accEm="hsasdfg"
+// const accId="12345"
+// var accPwd="qwerty"
+// let accState
+// console.table([accEm,accId,accPwd,accState]);
+// alert(6+8)
+
+
+let x=5
+console.log(x++);
+
+let a=8
+console.log(++a);
+
